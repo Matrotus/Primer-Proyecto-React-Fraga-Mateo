b4678af066b08f1f5d360adc36fa79ffd0001a30
Matrotus <mateofraga412@gmail.com>
2023-02-26 15:49:29 -0300

    vercel te amo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import ItemListContainer from './components/itemListContainer/ItemListContainer';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import Cart from './components/Cart/Cart.js';
